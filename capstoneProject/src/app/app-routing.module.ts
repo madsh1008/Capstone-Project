a3a6af9d20ecb0a1f8dda82f5e712b5718c37a1b
Madelyn Howes <madelynh33@gmail.com>
2023-10-05 13:47:31 -0500

    Added node server, set up components and routing

---
+++ capstoneProject/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { PostsPageComponent } from "./posts-page/posts-page.component";
+import { ProfilePageComponent } from "./profile-page/profile-page.component";
+
+//Routes to each component page
+const routes: Routes = [
+  {
+    path:'', component: HomePageComponent
+  },
+  {
+    path:'login', component: LoginPageComponent
+  },
+  {
+    path:'posts', component: PostsPageComponent
+  },
+  {
+    path:'profile', component: ProfilePageComponent
+  },
+];
+
+@NgModule({
+  imports:[RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
+
--- capstoneProject/src/app/app.module.ts
+++ capstoneProject/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { PostsPageComponent } from './posts-page/posts-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    ProfilePageComponent,
+    PostsPageComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
