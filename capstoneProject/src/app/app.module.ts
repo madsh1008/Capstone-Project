aceae3067b715d8fba6e3507c95a6cb2174e93d6
Sabrina Broadway <sabrinabroadway@sabrinas-mbp.lan>
2023-10-17 12:40:42 -0500

    Login page creation

--- capstoneProject/src/app/app.module.ts
+++ capstoneProject/src/app/app.module.ts
@@ -14,6 +14,14 @@ import { FooterComponent } from './footer/footer.component';
 //These are the imports for the angular material used
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input';
+import { MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component'
 
 @NgModule({
   declarations: [
@@ -24,12 +32,22 @@ import {MatButtonModule} from '@angular/material/button';
     PostsPageComponent,
     HeaderComponent,
     FooterComponent,
+    SignUpPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- capstoneProject/src/app/login-page/login-page.component.ts
+++ capstoneProject/src/app/login-page/login-page.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
 
-}
+    hide: boolean = false;
+  
+    constructor(private fb: FormBuilder) {
+    }
+  
+    ngOnInit() {
+    }
+  
+    loginForm: FormGroup = this.fb.group({
+      pawprint: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(12)]]
+    })
+  
+  
+    onLogin() {
+      if (!this.loginForm.valid) {
+        return;
+      }
+      console.log(this.loginForm.value);
+    }
+  
+  }
+
+
+
+
