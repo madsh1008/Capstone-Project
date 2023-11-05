689a0bcfbf31743616a2dc9f6bfd296db49fead7
Madelyn Howes <madelynh33@gmail.com>
2023-11-05 13:27:46 -0600

    Added more detail to post.

--- capstoneProject/src/app/posts-page/posts-page.component.ts
+++ capstoneProject/src/app/posts-page/posts-page.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./posts-page.component.css']
 })
 export class PostsPageComponent {
-
+  user = "user1"
+  avatar = 'https://material.angular.io/assets/img/examples/shiba1.jpg'
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  image = 'https://material.angular.io/assets/img/examples/shiba2.jpg'
+  status = 'Pending'
 }
