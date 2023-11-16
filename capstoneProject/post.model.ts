c685a8073e76a5b6d1a5ac8090ca1216f3671647
Sabrina Broadway <sabrinabroadway@Sabrinas-MBP.lan>
2023-11-15 18:55:56 -0600

    Removed Sweetheart

---
+++ capstoneProject/post.model.ts
@@ -0,0 +1,11 @@
+export interface Post {
+
+    post_id: String,
+    user_id: String,
+    post_title: String,
+    description: String,
+    pictures: File,
+    trade_status: String,
+    free_status: String
+    
+}
