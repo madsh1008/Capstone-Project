127ec19025f86cb8bd6beb4f0572c6aaf9ba59c3
Sabrina Broadway <sabrinabroadway@Sabrinas-MBP.lan>
2023-11-27 04:16:46 -0600

    Front-End Work

--- capstoneProject/src/app/footer/footer.component.ts
+++ capstoneProject/src/app/footer/footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+
+  }
 
 }
