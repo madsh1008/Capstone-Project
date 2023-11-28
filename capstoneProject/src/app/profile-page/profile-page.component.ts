47d225787ba7b4f7df0f4249843728f3d21ac0e2
Sabrina Broadway <sabrinabroadway@Sabrinas-MBP.lan>
2023-11-28 05:16:45 -0600

    More Front-End Work

--- capstoneProject/src/app/profile-page/profile-page.component.ts
+++ capstoneProject/src/app/profile-page/profile-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.css']
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit {
+
+  profileJson: string | null = null;
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe(
+      (profile) => (this.profileJson = JSON.stringify(profile, null, 2))
+    )
+  }
 
 }
